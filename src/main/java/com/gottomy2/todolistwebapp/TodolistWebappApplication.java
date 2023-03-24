d49808ca3a187a88148c534a8ed4d58d6421687a
gottomy2 <igottomy@gmail.com>
2023-03-24 14:19:32 +0100

    update: initial commit

---
+++ src/main/java/com/gottomy2/todolistwebapp/TodolistWebappApplication.java
@@ -0,0 +1,13 @@
+package com.gottomy2.todolistwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gottomy2/todolistwebapp/TodolistWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gottomy2.todolistwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
