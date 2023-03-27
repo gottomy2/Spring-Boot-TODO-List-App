cca55131317ca2909506ad9b2ce41fc031124b45
gottomy2 <igottomy@gmail.com>
2023-03-27 18:21:07 +0200

    update: replaced hardcoded code from findByUsername, and fixed typos

--- src/main/java/com/gottomy2/todolistwebapp/todo/TodoService.java
+++ src/main/java/com/gottomy2/todolistwebapp/todo/TodoService.java
@@ -14,13 +14,14 @@ public class TodoService {
     private static int todosCount = 0;
 
     static {
-        todos.add(new Todo(++todosCount, "gottomy", "Learn Spring Boot", LocalDate.now().plusMonths(2), false));
-        todos.add(new Todo(++todosCount, "gottomy", "Learn Fullstack Development", LocalDate.now().plusYears(1), false));
-        todos.add(new Todo(++todosCount, "gottomy", "Do Laundry", LocalDate.now().plusDays(1), false));
+        todos.add(new Todo(++todosCount, "gottomy2", "Learn Spring Boot", LocalDate.now().plusMonths(2), false));
+        todos.add(new Todo(++todosCount, "gottomy2", "Learn Fullstack Development", LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(++todosCount, "gottomy2", "Do Laundry", LocalDate.now().plusDays(1), false));
     }
 
     public List<Todo> findByUsername(String username) {
-        return todos;
+        Predicate<? super Todo> predicate = todos -> todos.getUsername().equalsIgnoreCase(username);
+        return todos.stream().filter(predicate).toList();
     }
 
     public void addTodo(String username, String description, LocalDate targetDate, boolean done){
