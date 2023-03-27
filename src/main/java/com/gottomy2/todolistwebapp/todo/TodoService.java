309a0abeff0a610871f7a3bb1654a8d6fc82da32
gottomy2 <igottomy@gmail.com>
2023-03-27 14:44:20 +0200

    update: added ability to delete TODO from list

--- src/main/java/com/gottomy2/todolistwebapp/todo/TodoService.java
+++ src/main/java/com/gottomy2/todolistwebapp/todo/TodoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Service
 public class TodoService {
@@ -25,5 +26,11 @@ public class TodoService {
     public void addTodo(String username, String description, LocalDate targetDate, boolean done){
         Todo todo = new Todo(++todosCount, username, description, targetDate, done);
         todos.add(todo);
+    }
+
+    public void deleteById(int id){
+        //If todo from todosList id has matching id remove it:
+        Predicate<? super Todo> predicate = todos -> todos.getId() == id;
+        todos.removeIf(predicate);
     }
 }
